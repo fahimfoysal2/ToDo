d3140446cb34a5590e86cbfdb26a6d6adb25bd8a
Fahim Foysal <ffoysal6@gmail.com>
2021-06-30 12:14:53 +0600

    APP layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>Laravel</title>
+@section('title', 'ToDo')
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-</head>
-<body>
-    <div id="app" class="mt-4">
-{{--        {{dd($todos)}}--}}
-        <todo-list :fetched-todos="{{$todos}}"></todo-list>
-    </div>
-</body>
-</html>
+@section('content')
+    <todo-list :fetched-todos="{{$todos}}"></todo-list>
+@endsection
